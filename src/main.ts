3163d86bcea1704f102c93c63c5f150046272ff7
mioe <mioe.boss@gmail.com>
2022-04-30 00:54:03 +0300

    feat: add checkbox

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import 'virtual:windi.css'
 import './assets/sass/main.sass'
 
 /**
+ * Windi themes
+ */
+export { default as OhMyProject } from './assets/windi-themes/oh-my-project/index'
+
+/**
  * Export components
  */
-export { default as HelloWorld } from './components/HelloWorld.vue'
+export { default as Checkbox } from './components/Checkbox/Checkbox.vue'
