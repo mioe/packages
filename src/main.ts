b1c574c03d55c766005ea4069979ccc13940e51b
mioe <mioe.boss@gmail.com>
2022-04-30 17:24:22 +0300

    feat: add radio

--- src/main.ts
+++ src/main.ts
@@ -12,5 +12,5 @@ export { default as OhMyProject } from './assets/windi-themes/oh-my-project/inde
 /**
  * Export components
  */
-import Checkbox from './components/Checkbox/Checkbox.vue'
-export { Checkbox }
+export { default as Checkbox } from './components/Checkbox/Checkbox.vue'
+export { default as Radio } from './components/Radio/Radio.vue'
