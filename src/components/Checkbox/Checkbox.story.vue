1a9c1e07107b7d0719743db87c934bc2e7d00882
mioe <mioe.boss@gmail.com>
2022-06-07 22:48:09 +0300

    add stories

---
+++ src/components/Checkbox/Checkbox.story.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import 'virtual:windi.css'
+import '../../assets/sass/main.sass'
+import Checkbox from './Checkbox.vue'
+</script>
+
+<template>
+	<Story title="MyStory">
+		<Variant title="MyVariant 1">
+			<Checkbox argument="hello" />
+		</Variant>
+		<Variant title="MyVariant 2">
+			<Checkbox>
+				<span class="ml-[20px]">
+					dd
+				</span>
+			</Checkbox>
+		</Variant>
+	</Story>
+</template>
---
+++ src/components/Introduction.story.vue
@@ -0,0 +1,13 @@
+<template>
+	<Story
+		group="top"
+		docs-only
+		icon="carbon:bookmark"
+	/>
+</template>
+
+<docs lang="md">
+# Welcome
+
+> simple components
+</docs>
