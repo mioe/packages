e8c56cc0eb115ad8ae2a768a4806be11c5076430
mioe <mioe.boss@gmail.com>
2022-07-29 19:40:38 +0300

    add color brand

--- src/histoire.setup.ts
+++ src/histoire.setup.ts
@@ -1,8 +1,8 @@
-import 'virtual:windi.css'
 import './assets/sass/main.sass'
 
 import { defineSetupVue3 } from 'histoire/client'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const setupVue3 = defineSetupVue3(({ app }) => {
 	console.log('🦕 init defineSetupVue3')
 })
